143838b862299d08e80b15fcfd6c06d0e72c9ca7
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-08-10 13:29:32 +0300

    fixed

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='secret0password')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['foodrgam.ddns.net', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
