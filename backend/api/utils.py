b74cc9f0f1ef7f3524d8ad519824c2a742133024
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-08-11 10:21:50 +0300

    test dowland

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -18,15 +18,18 @@ def download_shopping_cart(request):
     ).values(
         'ingredient__name',
         'ingredient__measurement_unit'
-    ).annotate(amount=Sum('amount'))
-    shopping_list += '\n'.join([
-        f'- {item["ingredient__name"]} '
-        f'({item["ingredient__measurement_unit"]})'
-        f' - {item["amount"]}'
-        for item in ingredients_cart
-    ])
+    ).annotate(amount=Sum('amount')
+               ).order_by()
+    for number, item in enumerate(ingredients_cart):
+        shopping_list += (
+            f'\n- {item["ingredient__name"]} '
+            f'({item["ingredient__measurement_unit"]})'
+            f' - {item["amount"]}'
+        )
+        if number < ingredients_cart.count() - 1:
+            shopping_list += ', '
     shopping_list += '\n\nFoodgram'
     filename = 'shopping_list.pdf'
-    response = HttpResponse(shopping_list, content_type='text/csv')
+    response = HttpResponse(shopping_list, 'Content-Type: application/pdf')
     response['Content-Disposition'] = f'attachment; filename={filename}'
     return response
